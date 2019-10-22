be08b14f48c14c267f6aa67ffa7366e625098ce8
Sami Aich <saich@student.42.fr>
2019-10-22 18:48:15 +0200

    strl change

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,31 +6,29 @@
 /*   By: saich <saich@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 12:51:02 by saich             #+#    #+#             */
-/*   Updated: 2019/10/21 16:15:20 by saich            ###   ########.fr       */
+/*   Updated: 2019/10/22 18:47:08 by saich            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t		ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 	size_t	j;
-	size_t	k;
 
-	i = 0;
 	j = 0;
-	k = 0;
-	while (dst[i] != 0)
-		i++;
-	while (src[j] != 0)
+	if (dstsize == 0)
+		return (0);
+	while (dst[j] && j < dstsize)
 		j++;
+	i = j;
+	while (j < dstsize - 1 && src[j - i])
+	{
+		dst[j] = src[j - i];
+		j++;
+	}
 	if (i < dstsize)
-		j = i + j;
-	else
-		j = j + dstsize;
-	while (src[k] != '\0' && i + 1 < dstsize)
-		dst[i++] = src[k++];
-	dst[i] = '\0';
-	return (j);
+		dst[j] = '\0';
+	return (i + ft_strlen(src));
 }
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,27 +6,26 @@
 /*   By: saich <saich@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 12:51:23 by saich             #+#    #+#             */
-/*   Updated: 2019/10/22 17:05:40 by saich            ###   ########.fr       */
+/*   Updated: 2019/10/22 18:36:56 by saich            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstszie)
 {
-	size_t i;
+	size_t	i;
 
 	i = 0;
-	if (!dest || !src)
+	if (!(src))
 		return (0);
-	while (dstsize > 0 && i < dstsize - 1 && src[i] != 0)
+	if (dstszie == 0)
+		return (ft_strlen(src));
+	while (i < dstszie - 1 && src[i])
 	{
-		dest[i] = src[i];
+		dst[i] = src[i];
 		i++;
 	}
-	if (dstsize != 0)
-		dest[i] = 0;
-	while (src[i])
-		i++;
-	return (i);
+	dst[i] = '\0';
+	return (ft_strlen(src));
 }
